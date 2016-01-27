da8abb9af6402b62f28d3ea21179affd2fd1bca0
Jordan Francis <jordan.francis@atos.net>
2016-01-27 10:44:30 +0000

    [task 100230, created status entity and status dao as well as junit, null]

--- svc/app/models/persistence/entities/Status.java
+++ svc/app/models/persistence/entities/Status.java
@@ -5,4 +5,5 @@ import javax.persistence.*;
 @Entity
 @Table(name = "dbo.T_REF_DATA")
 @DiscriminatorValue("STATUS")
+@DiscriminatorColumn(name = "refDataType")
 public class Status extends ValueObject {}
