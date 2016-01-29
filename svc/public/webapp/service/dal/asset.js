c4078e643a3b776fef9fb4e70676a6e210cf0fe6
coleycombie <nicole.mccombie@atos.net>
2016-01-29 10:23:40 +0000

    [Task 100026, DAL files created for manage Assets component]

--- svc/public/webapp/service/dal/asset.js
+++ svc/public/webapp/service/dal/asset.js
@@ -6,5 +6,21 @@
 
     function AssetDal (serviceCaller) {
         $log.debug("Instantiated assetDal");
+
+        this.getAsset = function () {
+            return service-caller.http.GET("asset");
+        };
+
+        this.saveAsset = function (assetToSave) {
+            return service-caller.http.POST("asset", assetToSave);
+        };
+
+        this.updateAsset = function (assetToUpdate) {
+            return service-caller.http.PUT("asset/", assetToUpdate);
+        };
+
+        this.deleteAsset = function (assetToDelete) {
+            return service-caller.http.DELETE("asset/", assetToDelete);
+        };
     }
 }());
